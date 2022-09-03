47765137b4e97f5db1aa33d0c75e6443c798d73f
Dan Davis <dan@danizen.net>
2022-09-03 11:34:51 -0400

    Get started using a remote tfstate

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,12 +1,13 @@
 # Configure the Azure provider
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
   }
-  required_version = ">= 1.1.0"
+  backend "local" {}
 }
 
 provider "azurerm" {
@@ -14,51 +15,51 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name        = var.resource_group_name
-  location    = var.region_name
+  name     = var.resource_group_name
+  location = var.region_name
 
   tags = {
     Environment = "Terraform Bootstrap"
-    Team = "DevOps"
+    Team        = "DevOps"
   }
 }
 
 # Create a storage account for application artifacts
 resource "azurerm_storage_account" "artifacts" {
-  name                      = "sadanizenartifacts"
-  resource_group_name       = azurerm_resource_group.rg.name
-  location                  = azurerm_resource_group.rg.location
-  account_tier              = "Standard"
-  account_replication_type  = "GRS"
+  name                     = "sadanizenartifacts"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 }
 
 # Create a container for that account
 resource "azurerm_storage_container" "apps" {
-  name                      = "danizencfgapps"
-  storage_account_name      = azurerm_storage_account.artifacts.name
-  container_access_type     = "private"
+  name                  = "apps"
+  storage_account_name  = azurerm_storage_account.artifacts.name
+  container_access_type = "private"
 }
 
 # Create a container for that account
 resource "azurerm_storage_container" "functions" {
-  name                      = "danizencfgfunctions"
-  storage_account_name      = azurerm_storage_account.artifacts.name
-  container_access_type     = "private"
+  name                  = "functions"
+  storage_account_name  = azurerm_storage_account.artifacts.name
+  container_access_type = "private"
 }
 
 # Create a storage account for terraform
 resource "azurerm_storage_account" "tf" {
-  name                      = "sadanizentf"
-  resource_group_name       = azurerm_resource_group.rg.name
-  location                  = azurerm_resource_group.rg.location
-  account_tier              = "Standard"
-  account_replication_type  = "GRS"
+  name                     = "sadanizentf"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 }
 
 # Create a container for terraform
 # Create a container for that account
 resource "azurerm_storage_container" "state" {
-  name                      = "danizentfstate"
-  storage_account_name      = azurerm_storage_account.tf.name
-  container_access_type     = "private"
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tf.name
+  container_access_type = "private"
 }
---
+++ vnet/main.tf
@@ -0,0 +1,16 @@
+# Configure the Azure provider
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
